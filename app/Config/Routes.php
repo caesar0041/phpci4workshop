1f9231479536da2927f7a85c0674fd7b4b60d372
Josephe-Anne Marie Misola <120217663+caesar0041@users.noreply.github.com>
2026-03-25 11:39:15 +0800

    modified gitignore file to ignore .env file | defined project routes

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,3 +6,17 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+
+$routes->group('tasks', function ($routes){
+   $routes->get('/' , 'TaskController::index');
+   $routes->get('show/(:num)' , 'TaskController::show/$1');
+   $routes->get('create/', 'TaskController::create');
+
+   $routes->post('store/', 'TaskController::store');
+
+   $routes->get('edit/(:num)', 'TaskController::edit/$1');
+
+   $routes->post('update/(:num)', 'TaskController::update/$1');
+   $routes->post('delete/(:num)', 'TaskController::delete/$1');
+
+});
